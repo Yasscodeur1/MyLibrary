af9fc4f770ee42653ae176e9a8a2887382288ba1
El yattouti <yasscodeur1.0@gmail.com>
2025-04-28 14:33:03 +0200

    verification de connexion1

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function welcome() {
   return (
-    <div>welcome</div>
+    <div>welcome
+        <h1>Liste de livres</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis vel tempora laudantium qui sed ea praesentium quasi eius ut a.</p>
+    </div>
   )
 }
