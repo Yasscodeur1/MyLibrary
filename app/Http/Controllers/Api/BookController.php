5ac9aff6fc51b9d7c9511a46eb04455b4c3796b6
El yattouti <yasscodeur1.0@gmail.com>
2025-04-28 16:12:25 +0200

    creation base backend configuration de blade et dashboard

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Book;
-use Illuminate\Http\Request;
 use App\Http\Requests\BookRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\BookResource;
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="p-6 bg-white border-b border-gray-200" id="app">
+        </div>
+    </div>
+</body>
+</html>
